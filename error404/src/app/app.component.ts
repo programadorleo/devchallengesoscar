945d6cbee04bf96bec230dda340742d90406f6b9
programadorleo <gandariasleo@gmail.com>
2020-09-23 16:54:16 -0300

    Angular error

--- error404/src/app/app.component.ts
+++ error404/src/app/app.component.ts
@@ -9,10 +9,14 @@ export class AppComponent {
   title = 'Error404';
   encabezado =  'Bienvenidos al error 404 ';
   subtitulo = 'Este es el error 404';
+  // Declaro un array de meses
+
+  meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
 
   // tslint:disable-next-line: typedef
   changeTitle(){
     this.title = 'Not Found Error 404';
+
   }
 
 }
